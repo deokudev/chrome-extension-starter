dd5a37c812901ff4890f6b41120b5cddc63c1d2b
sangdeok.jeong <sddev1002@lgcns.com>
2022-12-02 16:36:46 +0900

    fix bug

--- js/background.js
+++ js/background.js
@@ -1,60 +1,12 @@
-// Fired when the extension is first installed, when the extension is updated to a new version, and when Chrome is updated to a new version
-// http://developer.chrome.com/extensions/runtime.html#event-onInstalled
-chrome.runtime.onInstalled.addListener(function (details) {
-  // good place to set default options
-  function setDefaults(callback) {
-    storage.area.get(function (stored_options) {
-      var default_options = storage.default_options,
-        option,
-        new_options = {};
-      for (option in default_options) {
-        if (!stored_options.hasOwnProperty(option)) {
-          new_options[option] = default_options[option];
-        }
-      }
-      if (Object.keys(new_options).length !== 0) {
-        // save to area if new default options is appeared
-        storage.area.set(new_options, function () {
-          if (typeof callback === "function") {
-            callback();
-          }
-        });
-      } else {
-        if (typeof callback === "function") {
-          callback();
-        }
-      }
-    });
-  }
-
-  switch (details.reason) {
-    case "install": // if ext is  first installed
-      setDefaults(function () {
-        // show options page
-        chrome.tabs.create({ url: "options.html" });
-      });
-      break;
-    case "update":
-      setDefaults();
-      break;
-    default:
-      break;
-  }
-});
-
-chrome.runtime.onUpdateAvailable.addListener(function (details) {
-  // when an update is available - reload extension
-  // update will be install immediately
-  chrome.runtime.reload();
-});
+try {
+  importScripts("/js/background_storage.js", "/js/background_init.js");
+} catch (e) {
+  console.log(e);
+}
 
 // TO-DO : background에서 동작하는 js 코드를 작성
 // Receiving message from a content-script
-chrome.extension.onMessage.addListener(function (
-  request,
-  sender,
-  sendResponse
-) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.msg === "I'm content-script") {
     sendResponse({ answer: "OK! I'm background_page" });
   }
--- js/content_script.js
+++ js/content_script.js
@@ -2,7 +2,7 @@
 
 // Sending a request from a content script
 function requestMsgToBackground(msg) {
-  chrome.extension.sendMessage(
+  chrome.runtime.sendMessage(
     { msg: "I'm content-script" },
     function (response) {
       console.log(response);
@@ -11,11 +11,7 @@ function requestMsgToBackground(msg) {
 }
 
 // Receiving message from a background page
-chrome.extension.onMessage.addListener(function (
-  request,
-  sender,
-  sendResponse
-) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.msg === "Do you hear me?") {
     sendResponse({ answer: "Yes" });
   }
